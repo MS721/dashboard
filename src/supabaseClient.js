2dcc542865610fc57ae822effd30444eaf12e131
MS721 <hydra07689@gmail.com>
2025-11-15 12:10:19 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://yhupgiuuzhwucmwzfhai.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodXBnaXV1emh3dWNtd3pmaGFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NDg1NzAsImV4cCI6MjA3ODUyNDU3MH0.o2m2FZpODGYVIsxs_ZC1yGUoAbVv-YoyYvtHvwNdw2w"
+  import.meta.env.VITE_SUPABASE_URL,       
+  import.meta.env.VITE_SUPABASE_ANON_KEY  
 );
