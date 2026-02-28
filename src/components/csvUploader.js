12f4a6adf14432eff5a430b3ea21ecf083850193
MS721 <hydra07689@gmail.com>
2026-02-28 19:41:51 +0530

    Update csvUploader.js

--- src/components/csvUploader.js
+++ src/components/csvUploader.js
@@ -22,6 +22,7 @@ export async function uploadCSVToSupabase(file) {
       Papa.parse(file, {
         header: true,
         skipEmptyLines: true,
+        delimiter: ";", // Updated to semicolon based on your file
         complete: resolve,
         error: reject,
       });
@@ -39,24 +40,24 @@ export async function uploadCSVToSupabase(file) {
 
       const insertRow = {
         filename: file.name,
-        "Name": row.Name || row.name,
-        "Ph_no": row.Ph_no || row.ph_no,
-        "DATE_OF_SURVEY": row.DATE_OF_SURVEY || row.date,
-        "TIME_OF_SURVEY": row.TIME_OF_SURVEY || row.time,
-        "GPS_COORDINATES": row.GPS_COORDINATES || row.coordinates,
-        "STATE": row.STATE || row.state,
-        "DISTRICT": row.DISTRICT || row.district,
-        "TALUKA": row.TALUKA || row.taluka,
-        "VILLAGE": row.VILLAGE || row.village,
-        "GRID_ID": row.GRID_ID || row.grid_id,
-        "GCP_ID": row.GCP_ID || row.gcp_id,
-        "JULIFLORA_COUNT": num(row.JULIFLORA_COUNT || row.juliflora_count),
-        "OTHER_SPECIES_COUNT": num(row.OTHER_SPECIES_COUNT || row.other_species),
-        "JULIFLORA_DENSITY": num(row.JULIFLORA_DENSITY || row.juliflora_density),
+        "Name": row["Name"],
+        "Ph no": row["Ph no"],
+        "DATE OF SURVEY": row["DATE OF SURVEY"],
+        "TIME OF SURVEY": row["TIME OF SURVEY"],
+        "GPS COORDINATES": row["GPS COORDINATES"],
+        "STATE": row["STATE"],
+        "DISTRICT": row["DISTRICT"],
+        "TALUKA": row["TALUKA"],
+        "VILLAGE": row["VILLAGE"] || row["VILLAGES"], // Matches your CSV header 'VILLAGES'
+        "GRID ID": row["GRID ID"],
+        "GCP ID": row["GCP ID"],
+        "JULIFLORA COUNT": num(row["JULIFLORA COUNT"]),
+        "OTHER SPECIES COUNT": num(row["OTHER SPECIES COUNT"]),
+        "JULIFLORA DENSITY": num(row["JULIFLORA DENSITY"]),
         
         // Multiple photos split by space (Kobo default for single column export)
-        "PLANT_PHOTO": row.PLANT_PHOTO ? row.PLANT_PHOTO.split(" ") : [],
-        "ACKNOWLEDGEMENT": row.ACKNOWLEDGEMENT || row.acknowledgement,
+        "PLANT_PHOTO": row["PLANT PHOTO"] ? row["PLANT PHOTO"].split(" ") : [],
+        "ACKNOWLEDGEMENT": row["ACKNOWLEDGEMENT"],
         
         data: row,
       };
